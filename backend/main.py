bba5f429a0c0eea22e8129b780c68866d659db8c
guifeh <guigasparinofernandes@gmail.com>
2026-05-27 20:35:46 -0300

    adicionando urls corretas no cors

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,10 @@ app = FastAPI(title="LGPD Scanner API")
 # ---------------------------------------------------------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://lgpd-scanner.vercel.app",
+        "http://localhost:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
